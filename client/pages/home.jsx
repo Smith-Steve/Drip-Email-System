64d610380e1c7eba922fb41a1bafde7100cf1fb9
Smith-Steve <Steve.A.Dore@outlook.com>
2021-05-25 16:17:20 -0700

    first commit

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { incrementor: 0 };
+  }
+
+  handleClick() {
+    const newValue = this.state.incrementor + 1;
+    this.setState({ incrementor: newValue });
+  }
+
+  render() {
+
+    return (
+    <React.Fragment>
+      <div className="container">
+
+      </div>
+    </React.Fragment>
+    );
+  }
 }
+export default Home;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,77 @@
 require('dotenv/config');
 const express = require('express');
+const pg = require('pg');
+const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
 const app = express();
+app.use(express.json());
 
 app.use(staticMiddleware);
 
 app.use(errorMiddleware);
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+app.post('/api/contacts', (req, res, next) => {
+  const { firstName, lastName, company, email, phoneNumber } = req.body;
+  if (typeof firstName !== 'string' || typeof lastName !== 'string' || typeof company !== 'string' || typeof phoneNumber !== 'string') {
+    throw new ClientError('400', 'please enter text for all fields');
+  }
+  if (!firstName || !lastName || !company || !email || !phoneNumber) {
+    throw new ClientError('400', 'all fields are required in order to register a user.');
+  }
+  const sqlPostQuery = `insert into "contacts"
+                        ("firstName", "lastName", "company", "email", "phoneNumber")
+                        values ($1, $2, $3, $4, $5)
+                        returning*;`;
+  const sqlPostParams = [firstName, lastName, company, email, phoneNumber];
+  db.query(sqlPostQuery, sqlPostParams)
+    .then(result => {
+      const contact = result.rows[0];
+      res.status(201).json(contact);
+    }).catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'please see the entered parameters and try again.' });
+    });
+});
+
+app.get('/api/contacts/:contactId', (req, res, next) => {
+  const contactId = parseInt(req.params.contactId, 10);
+  if (!Number.isInteger(contactId) || contactId <= 0) {
+    throw new ClientError('400', 'must enter a legitimate contact.');
+  }
+  const sqlGetQuery = 'select * from "contacts" where "contactId" = $1';
+  const params = [contactId];
+  db.query(sqlGetQuery, params)
+    .then(result => {
+      const contact = result.rows[0];
+      res.status(200).json(contact);
+    }).catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'an unexpected error occurred.' });
+    });
+});
+
+app.delete('/api/contacts/:contactId', (req, res, next) => {
+  const contactId = parseInt(req.params.contactId, 10);
+  if (!Number.isInteger(contactId) || contactId <= 0) {
+    throw new ClientError('400', 'must enter a legitimate contact.');
+  }
+  const sqlDeleteQuery = 'delete from "contacts" where "contactId" = $1';
+  const deleteParams = [contactId];
+  db.query(sqlDeleteQuery, deleteParams)
+    .then(result => {
+      res.status(204).send();
+    }).catch(error => {
+      console.error(error);
+      res.status(500).json({ error: 'an unexpected error occured.' });
+    });
+});
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
