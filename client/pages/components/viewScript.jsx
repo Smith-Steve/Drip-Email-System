d21a428313c5707af8c542a0dd3603af1d2b5bf1
Smith-Steve <Steve.A.Dore@outlook.com>
2021-06-02 12:07:27 -0700

    removed constructor from viewScript component because it was not being used. Left viewScript as a class as opposed to a functional component because I anticipate more functionality out of it in the future.

--- client/pages/components/viewScript.jsx
+++ client/pages/components/viewScript.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
 class ViewScript extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { route: '', activeScript: null };
-  }
 
   render() {
     return (
