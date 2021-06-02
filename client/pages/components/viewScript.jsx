50023db73c3c4671d0c12ca94d5e9f14c4ad9096
Smith-Steve <Steve.A.Dore@outlook.com>
2021-06-02 12:05:32 -0700

    made minor changes to viewScript from last night, mostly in relation to the flexContainer element, so that the text within it and the button would respond to eachother. Also, made some CSS changes to aid in that.

--- client/pages/components/viewScript.jsx
+++ client/pages/components/viewScript.jsx
@@ -8,7 +8,7 @@ class ViewScript extends React.Component {
 
   render() {
     return (
-      <div className="manage-scripts">
+      <div className="view-script">
         <div className="row">
           <div className="col">
               <span className="specialText">Manage Script: {this.props.script.scriptName} </span>
@@ -17,12 +17,14 @@ class ViewScript extends React.Component {
         <div className="row">
           <div className="col">
             <div className="lg flexContainer">
-              <div className="row">
-                <span className="specialText">{this.props.script.scriptName} is a new script to which there are no e-mails attached</span>
+              <div className="row view-script">
+                <div className="col">
+                  <span className="specialText view-script">{this.props.script.scriptName} is a new script to which there are no e-mails attached</span>
+                  <div className="align-right">
+                    <button className="createEmail">Create Email</button>
+                  </div>
+                </div>
               </div>
-            <div className="align-right">
-              <button className="viewScripts">Create Email</button>
-            </div>
             </div>
           </div>
         </div>
