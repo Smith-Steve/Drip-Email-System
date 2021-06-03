b9beca7c6f19957220ba1ac9a47c8db81ca963a5
Smith-Steve <Steve.A.Dore@outlook.com>
2021-06-02 17:06:11 -0700

    made minor change. Something I over looked.

--- client/app.jsx
+++ client/app.jsx
@@ -4,8 +4,6 @@ import Home from './pages/home';
 export default class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
-          <Home/>
-      </React.Fragment>);
+          <Home/>);
   }
 }
