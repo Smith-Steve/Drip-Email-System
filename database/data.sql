eeca0225a1a482f97a5a1a97a0605f19c320042c
Smith-Steve <Steve.A.Dore@outlook.com>
2021-06-04 11:34:35 -0700

    almost lost these. Saving them

--- database/data.sql
+++ database/data.sql
@@ -2,4 +2,13 @@ insert into "contacts" ("firstName", "lastName", "company", "email", "phoneNumbe
 values ('Jeff', 'Lewis', 'Travel Co', 'JeffLewis@gmail.com', '7038914575');
 
 insert into "scripts" ("scriptName")
-values ('May BBM Script #1')
+values ('May BBM Script #1');
+
+insert into "flights" ("name", "topics", "scriptId")
+values ('May BBM flight #1', 'Nice', 1);
+
+insert into "emails" ("subject", "emailBody", "scriptId")
+values ('Data Update', 'Hello Blank, hope you are doing well. Sounds like you are having a great day today.', 1);
+
+insert into "flightAssignments" ("flightId", "contactId")
+values (1, 1);
