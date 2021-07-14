72b74e96c412ea4cef852060b37cfc85e5ef5e17
Smith-Steve <Steve.A.Dore@outlook.com>
2021-07-14 01:51:39 -0700

    able to send emails from multiple scripts from different module

---
+++ server/delayed-email-send-2.js
@@ -0,0 +1,82 @@
+require('dotenv/config');
+const pg = require('pg');
+const nodemailer = require('nodemailer');
+const connectionString = 'postgres://dev:dev@localhost/dripEmailDataBase';
+
+const transporter = nodemailer.createTransport({
+  pool: true,
+  host: 'smtp-mail.outlook.com',
+  secureConnection: false,
+  maxConnections: 1,
+  port: 587,
+  secure: false,
+  tls: { ciphers: 'SSLv3' },
+  auth: {
+    user: '',
+    pass: ''
+  }
+});
+
+const db = new pg.Pool({
+  connectionString: connectionString,
+  ssl: { rejectUnauthorized: false }
+});
+
+async function handleEmail2(flightInfo) {
+  const contactList = flightInfo.json_agg;
+  for (let i = 0; i < contactList.length; i++) {
+    const contact = contactList[i];
+    const messageBody = textCreator(flightInfo, contact);
+    const msg = {
+      from: process.env.EMAIL_USER,
+      to: contact.email,
+      subject: flightInfo.subject,
+      text: messageBody
+    };
+    await transporter.sendMail(msg).catch(error => {
+      console.error(error);
+    });
+  }
+}
+
+function textCreator(emailInfo, contact) {
+  const emailTextLines = emailInfo.emailBody.split('\n');
+  const emailArray = [];
+  for (let i = 0; i < emailTextLines.length; i++) {
+    const line = emailTextLines[i];
+    if (line.includes('{{PersonName}}')) {
+      const newLine = line.replace('{{PersonName}}', contact.firstName);
+      emailArray.push(newLine);
+    } else if (line.includes('')) {
+      const removedNonText = line.replace('', '\n');
+      emailArray.push(removedNonText);
+    } else {
+      emailArray.push(line);
+    }
+  }
+  const finalEmail = emailArray.join('');
+  return finalEmail;
+}
+
+const pgClient = new pg.Client(connectionString);
+pgClient.connect();
+
+const sqlEmailGetQuery = `select DISTINCT on ("f"."flightId") "f"."flightName" as "flightName", "s"."scriptName", "e"."subject", "e"."emailBody",
+                            (select json_agg (json_build_object('firstName', "c"."firstName", 'lastName', "c"."lastName", 'company',"c"."company", 'email', "c"."email"))
+                              from "contacts" as "c"
+                              inner join "flightAssignments" as "fas" on "c"."contactId" = "fas"."contactId"
+                              )
+                            from "flightAssignments" as "fA"
+                            join "contacts" as "c" on "fA"."contactId" = "c"."contactId"
+                            inner join "flights" as "f" on "fA"."flightId" = "f"."flightId"
+                            inner join "scripts" as "s" on "f"."scriptId" = "s"."scriptId"
+                            inner join "emails" as "e" on "s"."scriptId" = "e"."scriptId"
+                            where "e"."sendOn" > now()`;
+
+db.query(sqlEmailGetQuery)
+  .then(result => {
+    const flightInfo = result.rows[0];
+    handleEmail2(flightInfo);
+  }).catch(error => {
+    console.error(error);
+  });
--- server/delayed-email-send.js
+++ server/delayed-email-send.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const staticMiddleware = require('./static-middleware');
-const errorMiddleware = require('./lib/error-middleware');
-const db = require('./lib/database-config');
+// const db = require('./lib/database-config');
+require('dotenv/config');
 const handleEmail = require('./lib/handleEmail');
+const pg = require('pg');
 
-const app = express();
-app.use(express.json());
-app.use(staticMiddleware);
-app.use(errorMiddleware);
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
-const sqlEmailGetQuery = `select "f"."flightName" as "flightName", "s"."scriptName", "e"."subject", "e"."emailBody",
+const sqlEmailGetQuery = `select DISTINCT on ("f"."flightId") "f"."flightName" as "flightName", "s"."scriptName", "e"."subject", "e"."emailBody",
                             (select json_agg (json_build_object('firstName', "c"."firstName", 'lastName', "c"."lastName", 'company',"c"."company", 'email', "c"."email"))
                               from "contacts" as "c"
-                              inner join "flightAssignments" as "fa" on "c"."contactId" = "fa"."contactId"
+                              inner join "flightAssignments" as "fas" on "c"."contactId" = "fas"."contactId"
                               )
                             from "flightAssignments" as "fA"
                             join "contacts" as "c" on "fA"."contactId" = "c"."contactId"
                             inner join "flights" as "f" on "fA"."flightId" = "f"."flightId"
                             inner join "scripts" as "s" on "f"."scriptId" = "s"."scriptId"
                             inner join "emails" as "e" on "s"."scriptId" = "e"."scriptId"
-                            where "e"."sendOn" > now()`;
+                            where "e"."sendOn" > now() and "e"."sendOn" is NOT NULL`;
 db.query(sqlEmailGetQuery)
   .then(result => {
     const flightInfo = result.rows[0];
