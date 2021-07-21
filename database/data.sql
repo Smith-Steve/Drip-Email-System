6617fc12790985188d6777b46b106719a51be860
Smith-Steve <Steve.A.Dore@outlook.com>
2021-07-20 19:32:00 -0700

    update to database. Added more scripts so testing could be done more quickly.

--- database/data.sql
+++ database/data.sql
@@ -3,13 +3,14 @@ values ('Jeff', 'Lewis', 'Travel Co', 'Steve.A.Dore@outlook.com', '7038914575'),
 ('Thomas', 'Bill', 'Pinkerton Security', 'ThomasBill@dispostable.com', '2154981116'), ('Billy', 'Slick', 'Dispostable', 'BillySlick@dispostable.com', '2154981116');
 
 insert into "scripts" ("scriptName")
-values ('May BBM Script #1');
+values ('May BBM Script #1'),('July Script #1');
 
 insert into "flights" ("flightName", "topics", "scriptId")
-values ('May BBM flight #1', 'Nice', 1);
+values ('May BBM flight #1', 'Nice', 1),('Email #1', 'Nice', 2), ('Email #2', 'Nice', 2), ('Email #3', 'Nice', 2), ('Email #4', 'Nice', 2);
 
 insert into "emails" ("subject", "emailBody", "scriptId")
-values ('Penn Testing', 'Hello {{PersonName}}, Hope you are doing well. We are offering some new penn testing services. We feel this is a great opportunity for you and you all over at {{Company}} to have your software reviewed by some quality consultants.', 1);
+values ('Penn Testing', 'Hello {{PersonName}}, Hope you are doing well. We are offering some new penn testing services. We feel this is a great opportunity for you and you all over at {{Company}} to have your software reviewed by some quality consultants.', 1),
+('Penn Testing', 'Hello {{PersonName}}, Email 1', 2);
 
 insert into "flightAssignments" ("flightId", "contactId")
-values (1, 1);
+values (1, 1), (2, 2), (2, 3), (2, 4);
