9bc5bf34fd89029a55613a82a8e7c98c4e433a96
Smith-Steve <Steve.A.Dore@outlook.com>
2021-08-03 15:16:01 -0700

    Issue w number rendering on create email input table. Updated so numbering was accurate.

--- client/pages/components/scripts-folder/email-components/create-email.jsx
+++ client/pages/components/scripts-folder/email-components/create-email.jsx
@@ -62,7 +62,7 @@ class CreateEmail extends React.Component {
 
   renderSelectInput = emailNumber => {
     return (<div className="align-right">
-      <span className="special-text">E-mail Number#: {emailNumber}</span>
+      <span className="special-text">E-mail Number# {emailNumber + 1}</span>
     </div>);
   }
 
