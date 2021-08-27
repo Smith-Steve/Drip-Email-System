0035ed9708e7683950f4fa7119aa43d82e6d3bc0
Smith-Steve <Steve.A.Dore@outlook.com>
2021-08-27 16:50:39 -0700

    adding a count statement to delayed email send.

--- server/delayed-email-send.js
+++ server/delayed-email-send.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 const db = require('./lib/database-config');
 const handleEmail = require('./lib/handle-email');
 
-const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as "flightName", "e"."emailId" as "emailId", "s"."scriptName", "e"."subject", "e"."emailBody",
+const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as "flightName", "e"."emailId" as "emailId", "s"."scriptName", "e"."subject", "e"."emailBody", "e"."scriptId"
                             (select json_agg (json_build_object('firstName', "c"."firstName", 'lastName', "c"."lastName", 'company',"c"."company", 'email', "c"."email"))
                               from "contacts" as "c"
                               inner join "flightAssignments" as "fas" on "c"."contactId" = "fas"."contactId"
@@ -29,6 +29,14 @@ db.query(sqlEmailGetQuery)
       .catch(err => {
         console.error(err);
       });
+  }).then(() => {
+    const sqlCountQuery = `select
+                            count ("scriptId" = $2) as "Number of Scripts",
+                            count ("sentAt" is null) as "Number of Emails Not Sent"
+                          from "emails"
+                          where "scriptId" = $2`;
+    const param = [module.flightInformation.scriptId];
+    console.log(param);
   })
   .catch(error => {
     console.error(error);
