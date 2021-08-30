aae51be32158406ba4eca95e1d14c5e8a7f43b4f
Smith-Steve <Steve.A.Dore@outlook.com>
2021-08-30 11:43:12 -0700

    small change

--- server/delayed-email-send.js
+++ server/delayed-email-send.js
@@ -12,7 +12,7 @@ const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as
                             inner join "flights" as "f" on "fA"."flightId" = "f"."flightId"
                             inner join "scripts" as "s" on "f"."scriptId" = "s"."scriptId"
                             inner join "emails" as "e" on "s"."scriptId" = "e"."scriptId"
-                            where "e"."sendOn" < now() and "e"."sendOn" is NOT NULL and "e"."sentAt" is Null and "f"."flightComplete" = false`;
+                            where "e"."sendOn" > now() and "e"."sendOn" is NOT NULL and "e"."sentAt" is Null and "f"."flightComplete" = false`;
 
 db.query(sqlEmailGetQuery)
   .then(result => {
@@ -39,7 +39,7 @@ db.query(sqlEmailGetQuery)
     const param = [module.flightInformation.scriptId];
     db.query(sqlFlightCompleteQuery, param)
       .then(result => {
-        console.log(result);
+        // console.log(result);
       })
       .catch(err => {
         console.error(err);
