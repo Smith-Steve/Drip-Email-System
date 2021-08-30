5d2a6a27f62fd348c3c933ae04288743c0c1a3b4
Smith-Steve <Steve.A.Dore@outlook.com>
2021-08-29 17:51:39 -0700

    corrected error in delayed email send

--- server/delayed-email-send.js
+++ server/delayed-email-send.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 const db = require('./lib/database-config');
 const handleEmail = require('./lib/handle-email');
 
-const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as "flightName", "e"."emailId" as "emailId", "s"."scriptName", "e"."subject", "e"."emailBody", "e"."scriptId"
+const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as "flightName", "e"."emailId" as "emailId", "s"."scriptName", "e"."subject", "e"."emailBody", "e"."scriptId",
                             (select json_agg (json_build_object('firstName', "c"."firstName", 'lastName', "c"."lastName", 'company',"c"."company", 'email', "c"."email"))
                               from "contacts" as "c"
                               inner join "flightAssignments" as "fas" on "c"."contactId" = "fas"."contactId"
@@ -12,7 +12,7 @@ const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as
                             inner join "flights" as "f" on "fA"."flightId" = "f"."flightId"
                             inner join "scripts" as "s" on "f"."scriptId" = "s"."scriptId"
                             inner join "emails" as "e" on "s"."scriptId" = "e"."scriptId"
-                            where "e"."sendOn" < now() and "e"."sendOn" is NOT NULL and "e"."sentAt" is NULL and "f"."flightComplete" = false`;
+                            where "e"."sendOn" < now() and "e"."sendOn" is NOT NULL and "e"."sentAt" is Null and "f"."flightComplete" = false`;
 
 db.query(sqlEmailGetQuery)
   .then(result => {
@@ -30,18 +30,16 @@ db.query(sqlEmailGetQuery)
         console.error(err);
       });
   }).then(() => {
-
-    const sqlFlightCompleteQuery =
-                                  `select count ("scriptId" = $1) as "Number of Scripts", count ("sentAt" is null) as "Number of Emails Not Sent",
+    const sqlFlightCompleteQuery = `select count ("scriptId" = $1) as "Number of Scripts", count ("sentAt" is null) as "Number of Emails Not Sent",
                                     CASE WHEN count("sentAt") = 0 then true
                                   else false end
                                   as "flightComplete"
                                   from "emails"
-                                  where "scriptId" = 2`;
+                                  where "scriptId" = $1`;
     const param = [module.flightInformation.scriptId];
     db.query(sqlFlightCompleteQuery, param)
       .then(result => {
-        console.log(result);
+        // console.log(result);
       })
       .catch(err => {
         console.error(err);
