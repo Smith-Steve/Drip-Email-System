28d84aeea0915cc028f339bc45477ef07bc64171
Smith-Steve <Steve.A.Dore@outlook.com>
2021-09-13 13:46:18 -0700

    updating delayed send and index

--- server/delayed-email-send.js
+++ server/delayed-email-send.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 const db = require('./lib/database-config');
 const handleEmail = require('./lib/handle-email');
 
-const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as "flightName", "e"."emailId" as "emailId", "s"."scriptName", "e"."subject", "e"."emailBody", "e"."scriptId",
+const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as "flightName", "e"."emailId" as "emailId", "s"."scriptName", "e"."subject", "e"."emailBody", "f"."flightId", "e"."scriptId",
                             (select json_agg (json_build_object('firstName', "c"."firstName", 'lastName', "c"."lastName", 'company',"c"."company", 'email', "c"."email"))
                               from "contacts" as "c"
                               inner join "flightAssignments" as "fas" on "c"."contactId" = "fas"."contactId"
@@ -12,7 +12,7 @@ const sqlEmailGetQuery = `select DISTINCT on ("e"."emailId") "f"."flightName" as
                             inner join "flights" as "f" on "fA"."flightId" = "f"."flightId"
                             inner join "scripts" as "s" on "f"."scriptId" = "s"."scriptId"
                             inner join "emails" as "e" on "s"."scriptId" = "e"."scriptId"
-                            where "e"."sendOn" > now() and "e"."sendOn" is NOT NULL and "e"."sentAt" is Null and "f"."flightComplete" = false`;
+                            where "e"."sendOn" < now() and "e"."sendOn" is NOT NULL and "e"."sentAt" is Null and "f"."flightComplete" = false`;
 
 db.query(sqlEmailGetQuery)
   .then(result => {
@@ -39,7 +39,18 @@ db.query(sqlEmailGetQuery)
     const param = [module.flightInformation.scriptId];
     db.query(sqlFlightCompleteQuery, param)
       .then(result => {
-        // console.log(result);
+        const returnedInfo = result.rows[0];
+        if (!returnedInfo.flightComplete) {
+          const sqlUpdateQuery = `update "flights"
+             set "flightComplete" = true
+             where "flightId" = $1`;
+          // console.log(module.flightInformation.flightId);
+          const param = [module.flightInformation.flightId];
+          db.query(sqlUpdateQuery, param)
+            .catch(err => {
+              console.error(err);
+            });
+        }
       })
       .catch(err => {
         console.error(err);
